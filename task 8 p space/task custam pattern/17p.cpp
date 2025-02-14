c5f5d7428df7a21be7f1a4538c12f1f02517a042
Yogita Raval <148869324+ravalyogita15@users.noreply.github.com>
2025-02-14 08:23:35 +0530

    Add files via upload

---
+++ task 8 p space/task custam pattern/17p.cpp
@@ -0,0 +1,33 @@
+
+	//	* 
+	//	*        
+	//	*       
+	//	* 
+	//	*       
+	//	*       
+	//	* * * * 
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   
+	
+	for(int i=1; i<=7; i++)
+	{
+		for(int j=1; j<=4; j++)
+		{
+			if( i==7 || j==1 ) 
+			{
+				cout << "* ";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+}
