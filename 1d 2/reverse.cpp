51705269790c8cede9c7046e80737c00dece3ffe
Yogita Raval <148869324+ravalyogita15@users.noreply.github.com>
2025-02-20 16:35:41 +0530

    Add files via upload

---
+++ 1d 2/reverse.cpp
@@ -0,0 +1,23 @@
+// Write a program in C to read n number of values in an array and display them in reverse order.
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	int n;
+	cout << "Enter size of array: ";
+	cin >> n;
+	
+	int arr[n];
+	
+	for(int i=0;i<n;i++)
+	{
+		cout << "value of [" << i << "]:";
+		cin >> arr[i] ;
+	}
+	for(int i=n-1; i>=0; i--)
+	{
+		cout << arr[i] << " ";
+	}
+	cout <<endl;
+}
